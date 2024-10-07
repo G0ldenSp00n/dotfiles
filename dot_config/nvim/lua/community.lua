3717ae3a372edf8428d6923cd0bc553969106edb
G0ldenSp00n <hayfithyan@gmail.com>
2024-10-07 01:03:15 -0700

    feat: add kotlin, java packs

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -4,12 +4,14 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.recipes.vscode-icons" },
-  { import = "astrocommunity.recipes.telescope-lsp-mappings" },
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.lua" },
+	-- import/override with your plugins folder
+	{ import = "astrocommunity.colorscheme.catppuccin" },
+	{ import = "astrocommunity.pack.typescript" },
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.kotlin" },
+	{ import = "astrocommunity.pack.java" },
+	{ import = "astrocommunity.recipes.vscode-icons" },
+	{ import = "astrocommunity.recipes.telescope-lsp-mappings" },
 }
