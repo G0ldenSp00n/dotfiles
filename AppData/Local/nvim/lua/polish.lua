3b6da4290b44ea27529589faac5592e50d6a4ad2
G0ldenSp00n <hayfithyan@gmail.com>
2026-07-04 00:29:00 -0700

    feat(nvim): add F5 keymap for RemedyBG in polish.lua

--- AppData/Local/nvim/lua/polish.lua
+++ AppData/Local/nvim/lua/polish.lua
@@ -1,5 +1,20 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+-- Set up a mapping for C/C++ files to launch RemedyBG on <F5>
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp" },
+  callback = function()
+    vim.keymap.set("n", "<F5>", function()
+      local rdbg_files = vim.fn.glob("*.rdbg", false, true)
+      if #rdbg_files > 0 then
+        -- Open the first .rdbg session file found in the current directory
+        vim.cmd('silent !start remedybg.exe ' .. vim.fn.shellescape(rdbg_files[1]))
+      else
+        -- Just start remedybg if no session file exists
+        vim.cmd('silent !start remedybg.exe')
+      end
+    end, { buffer = true, desc = "Run in RemedyBG" })
+  end,
+})
